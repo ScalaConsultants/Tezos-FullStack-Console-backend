6d3631ee61d41b6720048481bf76a173633a7bb6
Piotr Sych <piotr.sych@adcolony.com>
2019-12-03 14:38:11 +0100

    MMT-22 | use proper scala-sbt docker image

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ docker run -i --rm \
     --name tezos-fullstack-console-backend \
     -v "$(pwd):/src" \
     -w /src \
-    hseeberger/scala-sbt:8u222_1.3.4_2.12.10 \
+    hseeberger/scala-sbt:11.0.4_1.3.4_2.12.10 \
     sbt clean assembly
 
 docker build -t tezos-fullstack-console-backend .
