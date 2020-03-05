9fc47f75237aeee937b0c71de28d5848cf4c2ec8
lk-schffr <47391260+lk-schffr@users.noreply.github.com>
2020-03-05 12:08:39 +0100

    [feature/MMT-120] Fix CORS configuration for local development
     * Fixed problem with missing local CORS configuration
     * Updated `run.sh` script, added BE and FE local development URIs for CORS
     * Updated and improved the `README.md` file

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-export EMAIL_USER="mmt-info@scalac.io" \
-export EMAIL_PASS=$1 \
+export EMAIL_USER="mmt-info@scalac.io"
+export EMAIL_PASS="$1"
 export EMAIL_RECEIVER="mmt-admin-dev@scalac.io"
+
+export FE_URL="http://localhost:3000"
+export BE_URL="http://localhost:30002"
 
 sbt clean assembly
 
